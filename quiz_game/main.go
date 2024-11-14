26a704a6bce6fa0e1d5b2fac22023a8e2c84381b
maxgonick <maxwellgonick@g.ucla.edu>
2024-11-13 17:48:34 -0800

    Part 2 of Quiz Game

--- quiz_game/main.go
+++ quiz_game/main.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
 	csvFile := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	timeLimit := flag.Int("timeLimit", 30, "a time limit for the quiz in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*csvFile)
@@ -27,13 +29,28 @@ func main() {
 	}
 	correct := 0
 	problems := parseLines(lines)
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
+	userInput := make(chan string)
+
 	for index, problem := range problems {
 		fmt.Printf("Problem %d: %s\n", index, problem.question)
-		var input string
-		fmt.Scanln(&input)
-		if input == problem.answer {
-			correct++
+		go func() {
+			var input string
+			fmt.Scanln(&input)
+			userInput <- input
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("You scored %v out of %v\n", correct, len(problems))
+			return
+		case answer := <-userInput:
+			if answer == problem.answer {
+				correct++
+			}
 		}
+
 	}
 	fmt.Printf("You scored %v out of %v\n", correct, len(problems))
 }
